79b7d3a774a14e410eb526bd6a7cf88c158f77e5
YegorCrazy <egor2002288@gmail.com>
2023-04-16 09:26:55 +0300

    some minor modifications to server

--- 06_SocialProject/cowsay_server.py
+++ 06_SocialProject/cowsay_server.py
@@ -9,53 +9,74 @@ async def chat(reader, writer):
     global clients
     global clients_names
     global available_names
-    me = "{}:{} appeared".format(*writer.get_extra_info('peername'))
-    print(me)
+    me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(f"{me} appeared")
     my_name = ''
     clients[me] = asyncio.Queue()
     send = asyncio.create_task(reader.readline())
     receive = asyncio.create_task(clients[me].get())
     while not reader.at_eof():
-        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        done, pending = await asyncio.wait([send, receive],
+                                           return_when=asyncio.FIRST_COMPLETED)
         try:
             for q in done:
                 if q is send:
                     send = asyncio.create_task(reader.readline())
-                    command = q.result().decode().split(maxsplit=1)
+                    command = q.result().decode()
+                    if len(command) == 0:
+                        continue
+                    print(command.strip())
+                    if command[0] == ' ':
+                        response_start_with = ' '
+                        command = command[1:]
+                    else:
+                        response_start_with, command = command.split(maxsplit=1)
+                        response_start_with += ' '
+                    command = command.split(maxsplit=1)
                     match command:
                         case ["who"]:
-                            await clients[me].put(', '.join(clients_names.keys()))
+                            await clients[me].put(response_start_with + ' '.join(clients_names.keys()))
                         case ["cows"]:
-                            await clients[me].put(', '.join(available_names))
+                            await clients[me].put(response_start_with + ' '.join(available_names))
                         case ["login", other]:
                             cow_name = other.strip()
                             if cow_name in available_names:
+                                if my_name != '':
+                                    available_names += [my_name]
+                                    del clients_names[my_name]
                                 clients_names[cow_name] = me
                                 my_name = cow_name
                                 available_names.remove(cow_name)
                                 print(f'{me} is logined as {my_name}')
+                                await clients[me].put(response_start_with +
+                                                      f'You are logined as {my_name}')
                             else:
-                                await clients[me].put('Name is not available')
+                                await clients[me].put(response_start_with + 'Name is not available')
                         case ['say', other]:
                             if my_name == '':
-                                await clients[me].put('You are not authorized')
+                                await clients[me].put(response_start_with + 'You are not authorized')
                             else:
                                 cow_name, message = other.split(maxsplit=1)
-                                message = cowsay.cowsay(message, cow=my_name)
-                                await clients[clients_names[cow_name]].put(message)
+                                if cow_name not in clients_names:
+                                    await clients[clients_names[cow_name]].put(response_start_with +
+                                                                               'No such cow')
+                                else:
+                                    message = cowsay.cowsay(message, cow=my_name)
+                                    await clients[clients_names[cow_name]].put(' ' + message)
                         case ['yield', message]:
                             if my_name == '':
-                                await clients[me].put('You are not authorized')
+                                await clients[me].put(response_start_with + 'You are not authorized')
                             else:
                                 message = cowsay.cowsay(message, cow=my_name)
                                 for client in clients.values():
                                     if client is not clients[me]:
-                                        await client.put(message)
+                                        await client.put(' ' + message)
                         case ['quit']:
                             available_names += [my_name]
                             del clients_names[my_name]
                             my_name = ''
                             print(f'{me} logined out')
+                            await clients[me].put(response_start_with + 'Logined out')
                 elif q is receive:
                     receive = asyncio.create_task(clients[me].get())
                     writer.write(f"{q.result()}\n".encode())
@@ -66,6 +87,9 @@ async def chat(reader, writer):
     receive.cancel()
     print(me, "DONE")
     del clients[me]
+    if my_name != '':
+        available_names += [my_name]
+        del clients_names[my_name]
     writer.close()
     await writer.wait_closed()
 
@@ -74,4 +98,7 @@ async def main():
     async with server:
         await server.serve_forever()
 
-asyncio.run(main())
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    print('Server down')
